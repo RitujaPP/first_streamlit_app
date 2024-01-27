36f0ea3bee4480b5915922b2fd376d7dfed8f04e
RitujaPP <157678755+RitujaPP@users.noreply.github.com>
2024-01-27 18:39:01 +0530

    Update streamlit_app.py
    typo of execute

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,31 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfru
 # streamlit.write('The user entered ', add_my_fruit)
 streamlit.write('Thanks for adding ', add_my_fruit) 
 
-my_cur.exceute("insert into fruit_load_list values ('from streamlit')")
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
