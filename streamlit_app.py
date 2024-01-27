13dd13590b0f1fd4aee8c504387e0271e4141e18
RitujaPP <157678755+RitujaPP@users.noreply.github.com>
2024-01-28 01:02:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ import snowflake.connector
 # streamlit.header("View Our Fruit List - Add Your Favourite")
 # streamlit.dataframe(my_data_rows)
 
-streamlit.header("View Our Fruit List - Add Your Favourite")
+streamlit.header("View Our Fruit List - Add Your Favourite!")
 #Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
