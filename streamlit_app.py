127ec0ca844547e2eecb58a7b25daba15eb10993
RitujaPP <157678755+RitujaPP@users.noreply.github.com>
2024-01-28 00:38:40 +0530

    Update streamlit_app.py
    fruit_choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,6 @@ except URLError as e:
   streamlit.error()
 
 import requests 
- 
-
 import snowflake.connector
 
 # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -68,7 +66,7 @@ def get_fruit_load_list():
 
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('" + new_fruit +"')")
+        my_cur.execute("insert into fruit_load_list values ('" + fruit_choice +"')")
         return "Thanks for adding "+ new_fruit
 
 # add a button to load the fruit
@@ -79,27 +77,25 @@ if streamlit.button('Get Fruit List'):
     streamlit.dataframe(my_data_rows)
  
 
-# Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  my_data_rows = get_fruit_load_list()
-  streamlit.dataframe(my_data_rows)
+# # Add a button to load the fruit
+# if streamlit.button('Get Fruit Load List'):
+#   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#   my_data_rows = get_fruit_load_list()
+#   streamlit.dataframe(my_data_rows)
 
 # streamlit.stop()
 
-#Text box 2
-def insert_row_snowflake(new_fruit):
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-    return "Thanks for adding " + new_fruit
+# #Text box 2
+# def insert_row_snowflake(new_fruit):
+#   with my_cnx.cursor() as my_cur:
+#     my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#     return "Thanks for adding " + new_fruit
  
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    back_from_function = insert_row_snowflake(add_my_fruit)
    streamlit.text(back_from_function)
-
- 
 
 streamlit.stop()
 
